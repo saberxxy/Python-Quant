e058d0901dc9bdef0031d80add2c438e745d8ace
yuanshuai <17600232953@163.com>
2017-07-25 18:42:56 +0800

    获取数据及部分处理

---
+++ yuanshuai/demo.py
@@ -0,0 +1,25 @@
+import tushare as ts
+import pandas as pd
+from pandas import DataFrame, Series
+
+
+# data = ts.get_industry_classified()
+# print(data[data['code'] == '000001'])
+# data = ts.get_stock_basics()
+# name = data[data.index=='000001'].name
+# print(type(name[0]))
+
+# data = ts.get_stock_basics()
+# df = data[data.index == '000001'].name
+# data.name = df
+# print(df)
+
+def main():
+    df = ts.get_h_data('000001', start='2017-01-01')
+    for i in range(df.shape[0]):
+        print(df.iloc[i], type(df.iloc[i].index))
+        print("----------------------------------")
+if __name__ == '__main__':
+    main()
+
+
--- yuanshuai/format_stock_data.py
+++ yuanshuai/format_stock_data.py
@@ -1,16 +1,27 @@
 import pandas as pd
 from pandas import DataFrame, Series
 import get_stock_data as gsd
+import uuid
+import random
 
 
 def format_data(raw_data):
     data = raw_data[0]
     code = raw_data[1]
+    # 添加列
     data['code'] = code
     data['name'] = gsd.get_name(code)
     data['industry'] = gsd.get_industry(code)
+    #去除date索引替换为uuid
+    rows = data.shape[0]
+    for d in range(rows):
+        randstr = ""
+        for i in range(5):
+            randstr = randstr,random.ranint(0, 9)
+            randstr = randstr,uuid.uuid1()
+        print(randstr, type(randstr))
+        print(data)
     return data
-
 
 def main():
     raw_data = gsd.get_data('000001')

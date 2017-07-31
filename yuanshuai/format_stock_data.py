8eb2f73c1b6ee6f03dbbce488638c4baef60834c
yuanshuai <18835170813@163.com>
2017-07-31 21:41:43 +0800

    up

--- yuanshuai/format_stock_data.py
+++ yuanshuai/format_stock_data.py
@@ -4,16 +4,21 @@ import get_stock_data as gsd
 import uuid
 import time
 
+
 def format_data(raw_data):
+    """
+    处理数据
+    :param raw_data:
+    :return:
+    """
     data = raw_data[0]
     code = raw_data[1]
     data['code'] = code
     data['name'] = gsd.get_name(code)
-    data['industry'] = gsd.get_industry(code)
     data['classify'] = gsd.get_type(code)
     # 添加一些列
-    data = DataFrame(data, columns=['uuid', 'date', 'code', 'name', 'industry', 'classify', 'open', 'close', 'high', 'low', 'volume', 'amount', \
-                                    'y_close', 'p_change', 'p_changerate'])
+    data = DataFrame(data, columns=['uuid', 'date', 'code', 'name', 'classify', 'open', 'close', 'high', 'low', 'volume', 'amount', \
+                                    'y_close', 'p_change', 'p_change_rate'])
     close_p = data['close']  # 收盘
     data['y_close'] = close_p.shift(-1)  # 昨收盘
 
@@ -22,7 +27,7 @@ def format_data(raw_data):
         data.loc[i, 'uuid'] = str(uuid.uuid1()) + "_" + data.loc[i, 'date']  # uuid
         # data.code = code  # 股票代码
         data.loc[i, 'p_change'] = data.loc[i, 'close'] - data.loc[i, 'y_close']  # 涨跌额
-        data.loc[i, 'p_changerate'] = data.loc[i, 'p_change'] / data.loc[i, 'y_close']  # 涨跌幅
+        data.loc[i, 'p_change_rate'] = data.loc[i, 'p_change'] / data.loc[i, 'y_close']  # 涨跌幅
         i = data.loc[i, 'uuid']
     return data
 

cd55979ac2363c0eaf5e5187573da5e526626c58
loony <eloony@163.com>
2017-09-12 07:21:59 -0700

    xx

--- GetStockData/GetAll.py
+++ GetStockData/GetAll.py
@@ -21,7 +21,7 @@ import os
 # 导入连接文件
 import sys
 sys.path.append("..")
-import common.GetOracleConn as conn
+import common.GetMysqlConn as conn
 
 # 获取全局数据库连接
 cursor = conn.getConfig()
@@ -33,13 +33,15 @@ def listStock(cursor):
     cursor.execute("select code from stock_basics where code like '6%'")
     pdata1 = cursor.fetchall()
     for i in pdata1:
-        dict1[i[0]] = 'http://quotes.money.163.com/service/chddata.html?code=0' + str(i[0]) + '&start=19900101&end=20171231'
+        print(i)
+        print(i["code"])
+        dict1[i["code"]] = 'http://quotes.money.163.com/service/chddata.html?code=0' + str(i["code"]) + '&start=19900101&end=20171231'
 
     dict2 = {}  # 存放开头不为6的股票代码及访问链接
     cursor.execute("select code from stock_basics where code not like '6%'")
     pdata2 = cursor.fetchall()
     for i in pdata2:
-        dict2[i[0]] = 'http://quotes.money.163.com/service/chddata.html?code=1' + str(i[0]) + '&start=19900101&end=20171231'
+        dict2[i["code"]] = 'http://quotes.money.163.com/service/chddata.html?code=1' + str(i["code"]) + '&start=19900101&end=20171231'
 
     # 合并两个字典
     dict3 = dict(dict1, **dict2)
@@ -48,17 +50,18 @@ def listStock(cursor):
 
 # 通过网易财经获取全量数据的CSV文件
 def getCSV(code, url):
-    fordername = 'AllStockData\\'
-    filename = str(code) + '.CSV'
-    with request.urlopen(url) as web:
-        # 为防止编码错误，使用二进制写文件模式
-        with open(fordername+filename, 'wb') as outfile:
-            outfile.write(web.read())
-
+#    fordername = 'AllStockData'+os.path.sep
+#    filename = str(code) + '.CSV'
+#    with request.urlopen(url) as web:
+#        # 为防止编码错误，使用二进制写文件模式
+#        print(web)
+#        with open(fordername+filename, 'wb') as outfile:
+#            outfile.write(web.read())
+#            print("write OK "+str(code))
     saveInDB(code)
     print("一只股票入库完毕")
     # 删除CSV文件
-    os.remove(fordername+filename)
+    #os.remove(fordername+filename)
 
 
 
@@ -66,12 +69,14 @@ def getCSV(code, url):
 def saveInDB(code):
     # 建表
     if not is_table_exist(code):  # 如果表不存在，先创建表
+        print("not_exist")
         create_table(code)  # 如果表不存在，先建表
     else:  # 存在则截断
+        print("exist")
         cursor.execute("truncate table stock_" + code)
 
     # 解析CSV文件并数据清洗
-    fordername = 'AllStockData\\'
+    fordername = 'AllStockData'+os.path.sep
     filename = str(code) + '.CSV'
     df = pd.read_csv(fordername+filename, encoding='gbk')
     df.rename(columns={u'日期': 'date', u'股票代码': 'code', u'名称': 'name', u'收盘价': 'close', u'最高价': 'high',
@@ -106,10 +111,32 @@ def saveInDB(code):
     try:
         for k in range(0, dfLen):
             df2 = df[k:k + 1]
-            sql = "insert into stock_"+str(code)+"(uuid, \"DATE\", code, name, classify, open, close, high, low," \
+            sql = "insert into stock_"+str(code)+"(uuid, `DATE`, code, name, classify, open, close, high, low," \
               "volume, amount, y_close, p_change, p_change_rate, turnover, marketcap, famc, zbs) " \
-              "values(:uuid, to_date(:datex, 'yyyy-MM-dd'), :code, :name, :classify, :open, :close, :high, :low, " \
-              ":volume, :amount, :y_close, :p_change, :p_change_rate, :turnover, :marketcap, :famc, :zbs)"
+              "values('%s', '%s', '%s', '%s', '%s', '%d', '%d', '%d', '%d', " \
+              "'%d', '%d' , '%d', '%d','%d', '%d','%d','%d','%d')"
+
+            print(  sql,
+                         ( str(list(df2['uuid'])[0]),
+                           str(list(df2['date'])[0]),
+                           str(list(df2['code'])[0]),
+                           str(list(df2['name'])[0]),
+                           str(list(df2['classify'])[0]),
+                           round(float(df2['open']), 4),
+                           round(float(df2['close']), 4),
+                           round(float(df2['high']), 4),
+                           round(float(df2['low']), 4),
+                           round(float(df2['volume']), 4),
+                           round(float(df2['amount']), 4),
+                           round(float(df2['y_close']), 4),
+                           round(float(df2['p_change']), 4),
+                           round(float(df2['p_change_rate']), 4),
+                           round(float(df2['turnover']), 4),
+                           round(float(df2['marketcap']), 4),
+                           round(float(df2['famc']), 4),
+                           round(float(df2['zbs']), 4)
+                         )
+                      )
 
             cursor.execute(  sql,
                          ( str(list(df2['uuid'])[0]),
@@ -133,8 +160,10 @@ def saveInDB(code):
                          )
                       )
             cursor.execute("commit")
-    except Exception:
+    except Exception as e:
         pass
+        print(e)
+
 
 
 
@@ -163,59 +192,65 @@ def get_type(code):
 def create_table(code):
     sql = "CREATE TABLE STOCK_" + code + """
     (
-            UUID VARCHAR2(80) PRIMARY KEY,
-            "DATE" DATE,
-            CODE VARCHAR2(20),
-            NAME VARCHAR2(80),
+            UUID VARCHAR(80) PRIMARY KEY,
+            `DATE` DATE,
+            CODE VARCHAR(20),
+            NAME VARCHAR(80),
             CLASSIFY VARCHAR(80),
-            OPEN NUMBER(20, 4),
-            CLOSE NUMBER(20, 4),
-            HIGH NUMBER(20, 4),
-            LOW NUMBER(20, 4),
-            VOLUME NUMBER(20, 4),
-            AMOUNT NUMBER(20, 4),
-            Y_CLOSE NUMBER(20, 4),
-            P_CHANGE NUMBER(20, 4),
-            P_CHANGE_RATE NUMBER(20, 4),
-            TURNOVER NUMBER(20, 4),
-            MARKETCAP NUMBER(30, 4),
-            FAMC NUMBER(30, 4),
-            ZBS NUMBER(20, 4)
+            OPEN decimal(20, 4),
+            CLOSE decimal(20, 4),
+            HIGH decimal(20, 4),
+            LOW decimal(20, 4),
+            VOLUME decimal(20, 4),
+            AMOUNT decimal(20, 4),
+            Y_CLOSE decimal(20, 4),
+            P_CHANGE decimal(20, 4),
+            P_CHANGE_RATE decimal(20, 4),
+            TURNOVER decimal(20, 4),
+            MARKETCAP decimal(30, 4),
+            FAMC decimal(30, 4),
+            ZBS decimal(20, 4)
         )
     """
+    print(sql)
     cursor.execute(sql)
     # 添加注释
-    comments = ["COMMENT ON TABLE STOCK_" + code + " IS '" + code + "'",  # 表注释
-                "COMMENT ON COLUMN STOCK_" + code + ".UUID IS 'UUID'",
-                "COMMENT ON COLUMN STOCK_" + code + ".\"DATE\" IS '日期'",
-                "COMMENT ON COLUMN STOCK_" + code + ".CODE IS '股票代码'",
-                "COMMENT ON COLUMN STOCK_" + code + ".NAME IS '股票名称'",
-                "COMMENT ON COLUMN STOCK_" + code + ".CLASSIFY IS '类别'",
-                "COMMENT ON COLUMN STOCK_" + code + ".OPEN IS '开盘价'",
-                "COMMENT ON COLUMN STOCK_" + code + ".CLOSE IS '收盘价'",
-                "COMMENT ON COLUMN STOCK_" + code + ".HIGH IS '最高价'",
-                "COMMENT ON COLUMN STOCK_" + code + ".LOW IS '最低价'",
-                "COMMENT ON COLUMN STOCK_" + code + ".VOLUME IS '成交量'",
-                "COMMENT ON COLUMN STOCK_" + code + ".AMOUNT IS '成交金额'",
-                "COMMENT ON COLUMN STOCK_" + code + ".Y_CLOSE IS '昨收盘'",
-                "COMMENT ON COLUMN STOCK_" + code + ".P_CHANGE IS '涨跌额'",
-                "COMMENT ON COLUMN STOCK_" + code + ".P_CHANGE_RATE IS '涨跌幅'",
-                "COMMENT ON COLUMN STOCK_" + code + ".TURNOVER IS '换手率'",
-                "COMMENT ON COLUMN STOCK_" + code + ".MARKETCAP IS '总市值'",
-                "COMMENT ON COLUMN STOCK_" + code + ".FAMC IS '流通市值'",
-                "COMMENT ON COLUMN STOCK_" + code + ".ZBS IS '成交笔数'" ]
-    for i in comments:
-        cursor.execute(i)
+#    comments = ["COMMENT ON TABLE STOCK_" + code + " IS '" + code + "'",  # 表注释
+#                "COMMENT ON COLUMN STOCK_" + code + ".UUID IS 'UUID'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".\"DATE\" IS '日期'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".CODE IS '股票代码'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".NAME IS '股票名称'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".CLASSIFY IS '类别'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".OPEN IS '开盘价'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".CLOSE IS '收盘价'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".HIGH IS '最高价'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".LOW IS '最低价'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".VOLUME IS '成交量'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".AMOUNT IS '成交金额'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".Y_CLOSE IS '昨收盘'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".P_CHANGE IS '涨跌额'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".P_CHANGE_RATE IS '涨跌幅'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".TURNOVER IS '换手率'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".MARKETCAP IS '总市值'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".FAMC IS '流通市值'",
+#                "COMMENT ON COLUMN STOCK_" + code + ".ZBS IS '成交笔数'" ]
+#    for i in comments:
+#       cursor.execute(i)
 
 
 # 判断表是否已存在
 def is_table_exist(code):
     table = "STOCK_" + code
-    sql = "select table_name from user_tables"
+    sql = "show tables like  '%s' " %table
     rs = cursor.execute(sql)
-    result = rs.fetchall()
-    tables = [i[0] for i in result]
-    return tables.__contains__(table)
+    result = cursor.fetchall()
+    print(result)
+    if len(result) > 0:
+        return True
+        ptint(table+"True")
+    else:
+        return False
+        ptint(table+"false")
 
 
 def main(key, url):

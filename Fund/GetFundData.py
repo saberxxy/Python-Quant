4d428f381d46ea5ca1d1b1b8085b95901006928d
saberxxy <albert_saber@163.com>
2018-05-01 15:18:13 +0800

    基金数据存入数据库

--- Fund/GetFundData.py
+++ Fund/GetFundData.py
@@ -11,12 +11,15 @@ import urllib.request
 import os
 import xml.sax
 import pandas as pd
+import uuid
 
-# # 基金代码
-# # 110022，180012，090013，320010，002001，003188
-# code = "090013"
-# url = "http://data.funds.hexun.com/outxml/detail/openfundnetvalue.aspx?" \
-#       "fundcode="+code+"&startdate=2018-01-01&enddate=2018-03-31"
+# 导入连接文件
+import sys
+sys.path.append("..")
+import common.GetOracleConn as conn
+
+# 获取全局数据库连接
+cursor = conn.getConfig()
 
 dcap = dict(DesiredCapabilities.PHANTOMJS)
 dcap["phantomjs.page.settings.userAgent"] = (
@@ -51,23 +54,91 @@ def saveCsv(soup):
         unitnetvalueList.append(float(x.find_all('fld_unitnetvalue')[0].contents[0]))
         netvalueList.append(float(x.find_all('fld_netvalue')[0].contents[0]))
     fundData = pd.DataFrame()
-    fundData['dateList'] = dateList
-    fundData['unitnetvalueList'] = unitnetvalueList
-    fundData['netvalueList'] = netvalueList
+    fundData['sdate'] = dateList
+    fundData['unitnetvalue'] = unitnetvalueList
+    fundData['netvalue'] = netvalueList
     # print(fundData.head())
     fundData.to_csv(code+'.csv', mode='w')
-    print('存储完毕')
+    print('存储CSV完毕')
+    return fundData
+
+# 存入数据库
+def save_db(code, fund_data):
+    # 建表
+    if not is_table_exist(code):  # 如果表不存在，先创建表
+        create_table(code)  # 如果表不存在，先建表
+    else:  # 存在则截断
+        cursor.execute("TRUNCATE TABLE FUND_" + code)
+
+    fund_data['uuid'] = [uuid.uuid1() for l in range(0, len(fund_data))]  # 添加uuid
+    fund_data['code'] = code
+    print(fund_data.head())
+    # 入库
+    for k in range(0, len(fund_data)):
+        df2 = fund_data[k:k + 1]
+        sql = "insert into fund_" + str(code) + \
+              "(uuid, sdate, code, unitnetvalue, netvalue) " \
+              "values(:uuid, to_date(:sdate, 'yyyy-MM-dd'), :code, " \
+              ":unitnetvalue, :netvalue)"
+
+        cursor.execute(sql,
+                       (
+                           str(list(df2['uuid'])[0]),
+                           str(list(df2['sdate'])[0]),
+                           str(list(df2['code'])[0]),
+                           round(float(df2['unitnetvalue']), 4),
+                           round(float(df2['netvalue']), 4)
+                       )
+                      )
+    cursor.execute("commit")
+
+    print("存入数据库")
+
+# 判断表是否已存在
+def is_table_exist(code):
+    table = "FUND_" + code
+    sql = "SELECT TABLE_NAME FROM USER_TABLES"
+    rs = cursor.execute(sql)
+    result = rs.fetchall()
+    tables = [i[0] for i in result]
+    print(table)
+    print(tables.__contains__(table))
+    return tables.__contains__(table)
+
+# 建表函数
+def create_table(code):
+    sql = "CREATE TABLE FUND_" + code + """
+    (
+            UUID VARCHAR2(80) PRIMARY KEY,
+            SDATE DATE,
+            CODE VARCHAR2(20),
+            UNITNETVALUE NUMBER(10,2),
+            NETVALUE NUMBER(10,2)
+        )
+    """
+    cursor.execute(sql)
+    # 添加注释
+    comments = ["COMMENT ON TABLE FUND_" + code + " IS '" + code + "'",  # 表注释
+                "COMMENT ON COLUMN FUND_" + code + ".UUID IS 'UUID'",
+                "COMMENT ON COLUMN FUND_" + code + ".SDATE IS '日期'",
+                "COMMENT ON COLUMN FUND_" + code + ".CODE IS '代码'",
+                "COMMENT ON COLUMN FUND_" + code + ".UNITNETVALUE IS '单位净值'",
+                "COMMENT ON COLUMN FUND_" + code + ".NETVALUE IS '累计净值'"]
+    for i in comments:
+        cursor.execute(i)
+
 
 if __name__ == '__main__':
     # 基金代码
     # 110022，180012，090013，320010，002001，003188
     codes = ['110022', '180012', '090013', '320010', '002001', '003188']
     for code in codes:
+    # code = '110022'
         url = "http://data.funds.hexun.com/outxml/detail/openfundnetvalue.aspx?" \
             "fundcode=" + code + "&startdate=2018-01-01&enddate=2018-03-31"
         soup = getSoup(url)
-        saveCsv(soup)
-
+        fund_data = saveCsv(soup)
+        save_db(code, fund_data)
 
 
 

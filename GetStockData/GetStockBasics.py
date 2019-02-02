cdb661202565e789efc40a9ef4000e7d917738a2
saberxxy <albert_saber@163.com>
2019-02-02 10:16:15 +0800

    更新获取stock basics新接口

--- GetStockData/GetStockBasics.py
+++ GetStockData/GetStockBasics.py
@@ -1,9 +1,11 @@
 # -*- coding=utf-8 -*-
 # 获取上市公司信息
+# 获取上市公司信息，更新新接口的信息
 
 import tushare as ts
 import cx_Oracle as cxo
 import configparser
+import uuid
 
 # 导入连接文件
 import sys
@@ -115,8 +117,65 @@ def main():
         getBasics(cursor)
 
 
+def main2():
+    pro = ts.pro_api()
+    df = pro.stock_basic(fields='ts_code, symbol, name, area, industry, fullname, \
+                                  enname, market, exchange, curr_type, list_status, \
+                                  list_date, delist_date, is_hs')
+    # print(df)
+    cursor = conn.getConfig()
+
+    ts_code = list(df['ts_code'])
+    symbol = list(df['symbol'])
+    name = list(df['name'])
+    area = list(df['area'])
+    industry = list(df['industry'])
+    fullname = list(df['fullname'])
+    enname = list(df['enname'])
+    market = list(df['market'])
+    exchange = list(df['exchange'])
+    curr_type = list(df['curr_type'])
+    list_status = list(df['list_status'])
+    list_date = list(df['list_date'])
+    delist_date = list(df['delist_date'])
+    is_hs = list(df['is_hs'])
+
+    dfLen = len(df)
+
+    cursor.execute("truncate table stock_basics_2")
+    print("清除成功")
+    for i in range(0, dfLen):
+        ts_code_db = str(ts_code[i])
+        symbol_db = str(symbol[i])
+        name_db = str(name[i])
+        area_db = str(area[i])
+        industry_db = str(industry[i])
+        fullname_db = str(fullname[i])
+        enname_db = str(enname[i]).replace("'", "")
+        market_db = str(market[i])
+        exchange_db = str(exchange[i])
+        curr_type_db = str(curr_type[i])
+        list_status_db = str(list_status[i])
+        list_date_db = str(list_date[i])
+        delist_date_db = str(delist_date[i])
+        is_hs_db = str(is_hs[i])
+
+        cursor.execute("insert into stock_basics_2 values('%s', '%s', \
+                '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', \
+                '%s', '%s', '%s', '%s')"
+                % (str(uuid.uuid1()), ts_code_db, symbol_db, name_db,
+                area_db, industry_db, fullname_db, enname_db, market_db,
+                exchange_db, curr_type_db, list_status_db, list_date_db,
+                delist_date_db, is_hs_db ))
+        cursor.execute("commit")
+        print("提交成功")
+
+
+
+
 if __name__ == '__main__':
     main()
+    main2()
 
 
 

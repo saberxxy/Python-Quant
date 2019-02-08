86f4e83eb457b43548e06cff38c4478a6180ea75
saberxxy <albert_saber@163.com>
2019-02-08 19:59:17 +0800

    计算mic、收益率、波动率

--- AnaStock/ca_return_vol.py
+++ AnaStock/ca_return_vol.py
@@ -10,29 +10,44 @@ import os
 import numpy as np
 import re
 import matplotlib.pyplot as plt
+from datetime import datetime
 # 导入连接文件
 import sys
 sys.path.append('..')
 import common.GetOracleConn as conn
 
 
-def get_stock_data(start_date, forder_name):
+def get_stock_data(start_date, end_date):
+	time_1 = time.time()
 	cursor = conn.getConfig()
-	sql_1 = "select table_name from user_tables where (table_name like 'STOCK_6%' \
-          or table_name like 'STOCK_0%' or table_name like 'STOCK_3') \
-          and table_name <> 'STOCK_000001_SSE' and substr(table_name, 7, 10) in \
-          (select symbol from stock_basics_2 where list_status='L') "
+	sql_1 = "select table_name from user_tables \
+			 where ((table_name like 'STOCK_6%' \
+             or table_name like 'STOCK_0%' or table_name like 'STOCK_3') \
+             and substr(table_name, 7, 10) in \
+             (select symbol from stock_basics_2 where list_status='L') ) \
+             or table_name = 'STOCK_000001_SSE' \
+             or table_name = 'STOCK_399001_SZSE' "
 	cursor.execute(sql_1)
 	res = cursor.fetchall()
 	res_list = []
+
+	# 获取库存指数最大日期
+	sql_2 = "select max(sdate) from stock_000001_sse"
+	cursor.execute(sql_2)
+	max_date = str(cursor.fetchall()[0][0])[0:10]
+	if end_date > max_date:
+		end_date = max_date
+	else:
+		end_date = end_date
+
 	for i in res:
-		time_1 = time.time()
 		res_list.append(i[0])
-		sql_2 = "select sdate, close, y_close from " + str(i[0]) + \
+		sql_3 = "select sdate, close, y_close from " + str(i[0]) + \
 				" where sdate >= to_date(\'" + start_date + "\', \'yyyy-MM-dd\') " + \
+				" and sdate <= to_date(\'" + end_date + "\', \'yyyy-MM-dd\') " + \
 				" and close > 0 "
 		# print(sql_2)
-		cursor.execute(sql_2)
+		cursor.execute(sql_3)
 		df = pd.DataFrame(cursor.fetchall(), columns=['sdate', 'close', 'y_close'])
 		df = df.dropna()
 		# 对数收盘价
@@ -53,15 +68,57 @@ def get_stock_data(start_date, forder_name):
 		df['stock_table_name'] = i[0]
 
 		# 保存结果
-		df.to_csv(forder_name + "\\" + i[0] + ".csv", index=False)
-		time_2 = time.time()
-		print(i[0], "	OK", time_2-time_1)
+		# df.to_csv(forder_name + "\\" + i[0] + "_" + start_date + ".csv", index=False)
+		df_stock_table_name = i[0]
+		df_stock_name = i[0][6:]
+		df_start_date = start_date  # 开始时间
+		df_end_date = end_date  # 结束时间
+		df_log_rate = round(float(str(np.sum(df['log_rate'])).replace('nan', '0')),4)  # 收益率
+		df_vol = round(float(str(np.average(df['volatility'])).replace('nan', '0')),4)  # 波动率
+		# print(df_stock_name)
+		# 入库
+		insert_db(df_stock_table_name, df_stock_name, df_start_date,
+			  df_end_date, df_log_rate, df_vol)
 
+		# print(df_stock_table_name, df_stock_name, df_start_date,
+		#  	  df_end_date, df_log_rate, df_vol)
+	time_2 = time.time()
+	print("OK", time_2-time_1)
+
+
+def insert_db(df_stock_table_name, df_stock_name, df_start_date,
+			  df_end_date, df_log_rate, df_vol):
+	cursor = conn.getConfig()
+	# print(df_stock_table_name, df_stock_name, df_start_date,
+	# 	  df_end_date, df_log_rate, df_vol)
+	cursor.execute("insert into stock_return_vol(uuid, stock_table_name, stock_code, start_date, \
+             end_date, log_rate, vol) values('%s', '%s', '%s', to_date('%s', 'yyyy-MM-dd'), \
+             to_date('%s', 'yyyy-MM-dd'), '%f', '%f')" % (
+		uuid.uuid1(), df_stock_table_name, df_stock_name, df_start_date,
+		df_end_date, df_log_rate, df_vol
+	))
+	cursor.execute("commit")
+
+	cursor.close()
+
+# 生成时间序列
+def datelist(beginDate, endDate):
+	# beginDate, endDate是形如‘20160601’的字符串或datetime格式
+	date_l = [datetime.strftime(x, '%Y-%m-%d') for x in list(pd.date_range(start=beginDate, end=endDate))]
+	return date_l
 
 if __name__ == '__main__':
-	start_date = '2018-01-01'
-	forder_name = "F:\\Program\\Python\\Quant\\AnaStock\\return_vol_results_" + \
-		start_date.replace('-', '')
-	os.makedirs(forder_name, exist_ok=True)
-	get_stock_data(start_date, forder_name)
+	# start_date = '2018-01-01'
+	cursor = conn.getConfig()
+	end_date = '2019-01-30'
+	sql_1 = "select max(end_date) from stock_return_vol"
+	cursor.execute(sql_1)
+	start_date = str(cursor.fetchall()[0][0])[0:10]
+
+	cursor.close()
+
+	end_date_list = datelist(start_date, end_date)
+	for end_date in end_date_list:
+		# print(end_date)
+		get_stock_data(start_date, end_date)
 
